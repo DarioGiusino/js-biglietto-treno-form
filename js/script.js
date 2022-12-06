d2a0e19d4df5c744e775a8821d0ba43a817f53e6
DarioGiusino <dario.giusino@gmail.com>
2022-12-06 16:05:56 +0100

    separated working html part, working on structure

--- js/script.js
+++ js/script.js
@@ -8,10 +8,16 @@ se utente minorenne
 se utente over 65
 4- calcolo prezzo -40%
 5* gestire casistiche (validazione)
-6- stampa in pagina
+6- stampa in pagina **DONE**
+
+MILESTONE 1:
+Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente due input e un bottone (non stilizzati), realizziamo le specifiche scritte sopra. La risposta finale (o output) sarà anch’essa da scrivere in console. **DONE**
+
+MILESTONE 2:
+Realizzare un form in pagina in cui l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo.
 */
 
-// Chiedo utente età e km e prendo elemento da riempire
+// get elements from DOM
 const userFinalPrice = document.getElementById('final-price');
 const userAge = document.getElementById('user-age');
 const travelKm = document.getElementById('kms');
@@ -19,27 +25,27 @@ const button = document.getElementById('generate');
 // // console.log(userFinalPrice, userAge, travelKm, button);
 
 button.addEventListener('click', function(){
-    // valore di età e km
+    // age and km values
     const valueAge = parseInt(userAge.value);
     const valueKm = parseInt(travelKm.value);
     // // console.log(valueAge, valueKm);
 
-    // età o km non validi
+    // age or km not valid
     if (isNaN(valueAge) || isNaN(valueKm) || valueAge === 0 || valueKm === 0){
         alert(`Per favore, inserisci numeri validi`)
     } else {    
-        // prezzo al km standard del biglietto
+        // ticket standard price
         let defaultPrice = (valueKm * 0.21).toFixed(2);
         // // console.log(defaultPrice);
         
-        // sconti relativi all'età
+        // discounts variables
         const littleDiscount = defaultPrice * 20 / 144;
         const underAge = 18;
         const bigDiscount = defaultPrice * 40 / 144;
         const overAge = 65;
         // // console.log(littleDiscount, bigDiscount);
-        
-        // calcoli utente minorenne/over 65
+
+        // discount math
         if (valueAge < underAge){
             defaultPrice = (defaultPrice - littleDiscount).toFixed(2);
         } else if (valueAge >= overAge){
@@ -47,7 +53,7 @@ button.addEventListener('click', function(){
         }
         // // console.log(defaultPrice);
         
-        // stampo su pagina
+        // print on page and console
         userFinalPrice.innerText = `€ ${defaultPrice}`;
         console.log(`€ ${defaultPrice}`);
     }
